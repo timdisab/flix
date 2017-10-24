5755bcfd0f582b9be166fc4b3ca220d14c87e6aa
Tim DiSabatino <tim@pathwaysdigital.com>
2017-10-24 09:45:41 -0700

    Add custom query for view index in model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   def flop?
     total_gross.blank? || total_gross < 50000000
   end
+
+  def self.released
+    @movies = Movie.where("released_on <= ?", Time.now).order('released_on desc')
+  end
 end
