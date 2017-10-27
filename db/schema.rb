3c600de22b3891f66f539ca8089fa5862f9c58ef
Tim DiSabatino <tim@pathwaysdigital.com>
2017-10-27 11:11:38 -0700

    Add new db fields, migrate & update files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017123234) do
+ActiveRecord::Schema.define(version: 20171027165142) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20171017123234) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.date "released_on"
+    t.string "cast"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: ""
   end
 
 end
