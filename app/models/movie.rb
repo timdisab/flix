2c59cbc40f17e66d29dce07906dff5b88b520c19
Tim DiSabatino <tim@pathwaysdigital.com>
2017-10-31 16:30:00 -0700

    Add validations at model, controller & view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,15 @@ class Movie < ApplicationRecord
   def self.released
     @movies = Movie.where("released_on <= ?", Time.now).order('released_on desc')
   end
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+  with:    /\w+\.(gif|jpg|png)\z/i,
+  message: "must reference a GIF, JPG, or PNG image"
+  }
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+  validates :rating, inclusion: { in: RATINGS }
 end
